a916e88c02039e3e93ce8105713d028c484e1e57
João Vitor Mendonça Merlin <95287980+jvopBR@users.noreply.github.com>
2025-11-04 15:57:31 -0300

    LoginPage

---
+++ nihon/src/app/admin/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const metadata = {
+  title: "Admin - Login",
+};
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
