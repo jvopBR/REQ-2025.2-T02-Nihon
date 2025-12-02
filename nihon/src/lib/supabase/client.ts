f1bbc27d5acf23453150954e6b5d218a32210e37
João Vitor Mendonça Merlin <95287980+jvopBR@users.noreply.github.com>
2025-12-02 00:35:14 -0300

    Fix formatting and add newline at end of client.ts

--- nihon/src/lib/supabase/client.ts
+++ nihon/src/lib/supabase/client.ts
@@ -15,9 +15,8 @@ export function supabaseUserClientSide() {
   );
 }
 
-
-export function supabaseAdmin()  {
-   return createClient(
+export function supabaseAdmin() {
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
